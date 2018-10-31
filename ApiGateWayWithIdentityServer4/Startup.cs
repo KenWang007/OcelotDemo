56f1e67d1178f63c458be8e69df11005d25519d0
Ken Wang <wolfmanlq@163.com>
2018-10-31 22:28:22 +0800

    Add demo code for intergate ocelot with identity server4

---
+++ ApiGateWayWithIdentityServer4/Startup.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+namespace ApiGateWayWithIdentityServer4
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddOcelot();
+            services.AddAuthentication()
+                .AddIdentityServerAuthentication("useridentity", options => {
+                    options.Authority = "http://localhost:5000";
+                    options.ApiName = "inventory";
+                    options.SupportedTokens = IdentityServer4.AccessTokenValidation.SupportedTokens.Both;
+                    options.ApiSecret = "inventorysecret";
+                    options.RequireHttpsMetadata = false;
+                });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseOcelot().Wait();
+        }
+    }
+}
